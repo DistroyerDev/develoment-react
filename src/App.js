41552f1d96ab9edd1bc16b14f219448c08deb3f5
daniel pedrozo <pedrozodaniel327@gmail.com>
2024-11-12 20:32:10 -0500

    codigo-poderoso

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DistroyerDev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Desarrollo Web Daniel Pedrozo
         </a>
       </header>
     </div>
